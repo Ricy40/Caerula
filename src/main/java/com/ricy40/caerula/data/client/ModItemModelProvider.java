775af036ac62b69ae46afe1000ea20734b57975b
Ricy40 <ricardogarcia.pt@outlook.com>
2022-04-24 19:41:13 +0100

    Nixium Progress...

--- src/main/java/com/ricy40/caerula/data/client/ModItemModelProvider.java
+++ src/main/java/com/ricy40/caerula/data/client/ModItemModelProvider.java
@@ -1,7 +1,10 @@
 package com.ricy40.caerula.data.client;
 
 import com.ricy40.caerula.Caerula;
+import com.ricy40.caerula.item.ModItems;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -16,19 +19,28 @@ public class ModItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
 
-        withExistingParent("nixium_block", modLoc("block/nixium_block"));
-        withExistingParent("raw_nixium_block", modLoc("block/raw_nixium_block"));
-        withExistingParent("nixium_ore", modLoc("block/nixium_ore"));
-        withExistingParent("deepslate_nixium_ore", modLoc("block/deepslate_nixium_ore"));
-        withExistingParent("nixium_slab", modLoc("block/nixium_slab"));
-        withExistingParent("nixium_stairs", modLoc("block/nixium_stairs"));
+        withExistingParent("nixium_block", modLoc("block/nixium/nixium_block"));
+        withExistingParent("raw_nixium_block", modLoc("block/nixium/raw_nixium_block"));
+        withExistingParent("nixium_ore", modLoc("block/nixium/nixium_ore"));
+        withExistingParent("deepslate_nixium_ore", modLoc("block/nixium/deepslate_nixium_ore"));
+        withExistingParent("nixium_slab", modLoc("block/nixium/nixium_slab"));
+        withExistingParent("nixium_stairs", modLoc("block/nixium/nixium_stairs"));
 
-        //ModelFile itemGenerated = getExistingFile(mcLoc("block/some_block"));
-        //builder(itemGenerated, "someblock");
+        simpleItem(ModItems.NIXIUM_INGOT.get());
+        simpleItem(ModItems.NIXIUM_NUGGET.get());
+        simpleItem(ModItems.RAW_NIXIUM.get());
 
     }
 
-    private ItemModelBuilder builder(ModelFile itemGenerated, String name) {
-        return getBuilder(name).parent(itemGenerated).texture("layer0", "item/" + name);
+    private ItemModelBuilder simpleItem(Item item) {
+        return withExistingParent(item.getRegistryName().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Caerula.MOD_ID,"item/" + item.getRegistryName().getPath()));
+    }
+
+    private ItemModelBuilder handheldItem(Item item) {
+        return withExistingParent(item.getRegistryName().getPath(),
+                new ResourceLocation("item/handheld")).texture("layer0",
+                new ResourceLocation(Caerula.MOD_ID,"item/" + item.getRegistryName().getPath()));
     }
 }
--- src/main/java/com/ricy40/caerula/data/client/ModRecipeProvider.java
+++ src/main/java/com/ricy40/caerula/data/client/ModRecipeProvider.java
@@ -4,27 +4,17 @@ import com.google.common.collect.ImmutableList;
 import com.ricy40.caerula.Caerula;
 import com.ricy40.caerula.block.ModBlocks;
 import com.ricy40.caerula.item.ModItems;
-import net.minecraft.advancements.critereon.*;
 import net.minecraft.core.Registry;
-import net.minecraft.data.BlockFamily;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.HoneycombItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SimpleCookingSerializer;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends RecipeProvider {
@@ -38,11 +28,15 @@ public class ModRecipeProvider extends RecipeProvider {
 
     protected void buildShapelessRecipes(Consumer<FinishedRecipe> consumer) {
 
-        ShapelessRecipeBuilder.shapeless(ModItems.NIXIUM_INGOT.get(), 9).requires(ModBlocks.NIXIUM_BLOCK.get()).unlockedBy("has_nixium_ingot", has(ModItems.NIXIUM_INGOT.get())).save(consumer);
-        ShapelessRecipeBuilder.shapeless(ModItems.NIXIUM_NUGGET.get(), 9).requires(ModItems.NIXIUM_INGOT.get()).unlockedBy("has_nixium_nugget", has(ModItems.NIXIUM_NUGGET.get())).save(consumer);
-
-        oreSmelting(consumer, NIXIUM_SMELTABLES, Items.IRON_INGOT, 0.7F, 200, "iron_ingot");
-        nineBlockStorageRecipes(consumer, Items.RAW_IRON, Items.RAW_IRON_BLOCK);
+        //ShapelessRecipeBuilder.shapeless(ModItems.NIXIUM_INGOT.get(), 9).requires(ModBlocks.NIXIUM_BLOCK.get()).unlockedBy("has_nixium_ingot", has(ModItems.NIXIUM_INGOT.get())).save(consumer);
+        //ShapelessRecipeBuilder.shapeless(ModItems.NIXIUM_NUGGET.get(), 9).requires(ModItems.NIXIUM_INGOT.get()).unlockedBy("has_nixium_nugget", has(ModItems.NIXIUM_NUGGET.get())).save(consumer);
+        oreSmelting(consumer, NIXIUM_SMELTABLES, ModItems.NIXIUM_INGOT.get(), 0.7F, 200, "nixium_ingot");
+        oreBlasting(consumer, NIXIUM_SMELTABLES, ModItems.NIXIUM_INGOT.get(), 0.7F, 100, "nixium_ingot");
+        nineBlockStorageRecipes(consumer, ModItems.RAW_NIXIUM.get(), ModBlocks.RAW_NIXIUM_BLOCK.get());
+        nineBlockStorageRecipesRecipesWithCustomUnpacking(consumer, ModItems.NIXIUM_INGOT.get(), ModBlocks.NIXIUM_BLOCK.get(), "nixium_ingot_from_nixium_block", "nixium_ingot");
+        nineBlockStorageRecipesWithCustomPacking(consumer, ModItems.NIXIUM_NUGGET.get(), ModItems.NIXIUM_INGOT.get(), "nixium_ingot_from_nuggets", "nixium_ingot");
+        slabBuilder(ModBlocks.NIXIUM_SLAB.get(), Ingredient.of(ModBlocks.NIXIUM_BLOCK.get())).unlockedBy("has_nixium_block", has(ModBlocks.NIXIUM_BLOCK.get())).save(consumer);
+        stairBuilder(ModBlocks.NIXIUM_STAIRS.get(), Ingredient.of(ModBlocks.NIXIUM_BLOCK.get())).unlockedBy("has_nixium_block", has(ModBlocks.NIXIUM_BLOCK.get())).save(consumer);
 
         //Check RecipeProvider
 
@@ -52,44 +46,44 @@ public class ModRecipeProvider extends RecipeProvider {
         return new ResourceLocation(Caerula.MOD_ID, path);
     }
 
-    private static void oneToOneConversionRecipe(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pResult, ItemLike pIngredient, @Nullable String pGroup) {
-        oneToOneConversionRecipe(pFinishedRecipeConsumer, pResult, pIngredient, pGroup, 1);
+    private static void oneToOneConversionRecipe(Consumer<FinishedRecipe> consumer, ItemLike pResult, ItemLike pIngredient, @Nullable String pGroup) {
+        oneToOneConversionRecipe(consumer, pResult, pIngredient, pGroup, 1);
     }
 
-    private static void oneToOneConversionRecipe(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pResult, ItemLike pIngredient, @Nullable String pGroup, int pResultCount) {
-        ShapelessRecipeBuilder.shapeless(pResult, pResultCount).requires(pIngredient).group(pGroup).unlockedBy(getHasName(pIngredient), has(pIngredient)).save(pFinishedRecipeConsumer, getConversionRecipeName(pResult, pIngredient));
+    private static void oneToOneConversionRecipe(Consumer<FinishedRecipe> consumer, ItemLike pResult, ItemLike pIngredient, @Nullable String pGroup, int pResultCount) {
+        ShapelessRecipeBuilder.shapeless(pResult, pResultCount).requires(pIngredient).group(pGroup).unlockedBy(getHasName(pIngredient), has(pIngredient)).save(consumer, getConversionRecipeName(pResult, pIngredient));
     }
 
-    private static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
+    private static void oreSmelting(Consumer<FinishedRecipe> consumer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(consumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
     }
 
-    private static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    private static void oreBlasting(Consumer<FinishedRecipe> consumer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(consumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
     }
 
-    private static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, SimpleCookingSerializer<?> pCookingSerializer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+    private static void oreCooking(Consumer<FinishedRecipe> consumer, SimpleCookingSerializer<?> pCookingSerializer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
         for(ItemLike itemlike : pIngredients) {
-            SimpleCookingRecipeBuilder.cooking(Ingredient.of(itemlike), pResult, pExperience, pCookingTime, pCookingSerializer).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike)).save(pFinishedRecipeConsumer, getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+            SimpleCookingRecipeBuilder.cooking(Ingredient.of(itemlike), pResult, pExperience, pCookingTime, pCookingSerializer).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike)).save(consumer, getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
         }
 
     }
 
-    private static void nineBlockStorageRecipes(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pUnpacked, ItemLike pPacked) {
-        nineBlockStorageRecipes(pFinishedRecipeConsumer, pUnpacked, pPacked, getSimpleRecipeName(pPacked), (String)null, getSimpleRecipeName(pUnpacked), (String)null);
+    private static void nineBlockStorageRecipes(Consumer<FinishedRecipe> consumer, ItemLike pUnpacked, ItemLike pPacked) {
+        nineBlockStorageRecipes(consumer, pUnpacked, pPacked, getSimpleRecipeName(pPacked), (String)null, getSimpleRecipeName(pUnpacked), (String)null);
     }
 
-    private static void nineBlockStorageRecipesWithCustomPacking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pUnpacked, ItemLike pPacked, String pPackingRecipeName, String pPackingRecipeGroup) {
-        nineBlockStorageRecipes(pFinishedRecipeConsumer, pUnpacked, pPacked, pPackingRecipeName, pPackingRecipeGroup, getSimpleRecipeName(pUnpacked), (String)null);
+    private static void nineBlockStorageRecipesWithCustomPacking(Consumer<FinishedRecipe> consumer, ItemLike pUnpacked, ItemLike pPacked, String pPackingRecipeName, String pPackingRecipeGroup) {
+        nineBlockStorageRecipes(consumer, pUnpacked, pPacked, pPackingRecipeName, pPackingRecipeGroup, getSimpleRecipeName(pUnpacked), (String)null);
     }
 
-    private static void nineBlockStorageRecipesRecipesWithCustomUnpacking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pUnpacked, ItemLike pPacked, String pUnpackingRecipeName, String pUnpackingRecipeGroup) {
-        nineBlockStorageRecipes(pFinishedRecipeConsumer, pUnpacked, pPacked, getSimpleRecipeName(pPacked), (String)null, pUnpackingRecipeName, pUnpackingRecipeGroup);
+    private static void nineBlockStorageRecipesRecipesWithCustomUnpacking(Consumer<FinishedRecipe> consumer, ItemLike pUnpacked, ItemLike pPacked, String pUnpackingRecipeName, String pUnpackingRecipeGroup) {
+        nineBlockStorageRecipes(consumer, pUnpacked, pPacked, getSimpleRecipeName(pPacked), (String)null, pUnpackingRecipeName, pUnpackingRecipeGroup);
     }
 
-    private static void nineBlockStorageRecipes(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ItemLike pUnpacked, ItemLike pPacked, String pPackingRecipeName, @Nullable String pPackingRecipeGroup, String pUnpackingRecipeName, @Nullable String pUnpackingRecipeGroup) {
-        ShapelessRecipeBuilder.shapeless(pUnpacked, 9).requires(pPacked).group(pUnpackingRecipeGroup).unlockedBy(getHasName(pPacked), has(pPacked)).save(pFinishedRecipeConsumer, new ResourceLocation(pUnpackingRecipeName));
-        ShapedRecipeBuilder.shaped(pPacked).define('#', pUnpacked).pattern("###").pattern("###").pattern("###").group(pPackingRecipeGroup).unlockedBy(getHasName(pUnpacked), has(pUnpacked)).save(pFinishedRecipeConsumer, new ResourceLocation(pPackingRecipeName));
+    private static void nineBlockStorageRecipes(Consumer<FinishedRecipe> consumer, ItemLike pUnpacked, ItemLike pPacked, String pPackingRecipeName, @Nullable String pPackingRecipeGroup, String pUnpackingRecipeName, @Nullable String pUnpackingRecipeGroup) {
+        ShapelessRecipeBuilder.shapeless(pUnpacked, 9).requires(pPacked).group(pUnpackingRecipeGroup).unlockedBy(getHasName(pPacked), has(pPacked)).save(consumer, new ResourceLocation(pUnpackingRecipeName));
+        ShapedRecipeBuilder.shaped(pPacked).define('#', pUnpacked).pattern("###").pattern("###").pattern("###").group(pPackingRecipeGroup).unlockedBy(getHasName(pUnpacked), has(pUnpacked)).save(consumer, new ResourceLocation(pPackingRecipeName));
     }
 
     private static String getHasName(ItemLike pItemLike) {
@@ -106,5 +100,13 @@ public class ModRecipeProvider extends RecipeProvider {
 
     private static String getConversionRecipeName(ItemLike pResult, ItemLike pIngredient) {
         return getItemName(pResult) + "_from_" + getItemName(pIngredient);
+    }
+
+    private static RecipeBuilder slabBuilder(ItemLike pSlab, Ingredient pMaterial) {
+        return ShapedRecipeBuilder.shaped(pSlab, 6).define('#', pMaterial).pattern("###");
+    }
+
+    private static RecipeBuilder stairBuilder(ItemLike pStairs, Ingredient pMaterial) {
+        return ShapedRecipeBuilder.shaped(pStairs, 4).define('#', pMaterial).pattern("#  ").pattern("## ").pattern("###");
     }
 }
