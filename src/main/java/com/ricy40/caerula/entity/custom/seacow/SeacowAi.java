e2612a7a2a3ba3b2d7bf61288991174b7d697926
Ricy40 <ricardogarcia.pt@outlook.com>
2022-07-09 00:26:13 +0100

    SeacowAi bug fixes

--- src/main/java/com/ricy40/caerula/entity/custom/seacow/SeacowAi.java
+++ src/main/java/com/ricy40/caerula/entity/custom/seacow/SeacowAi.java
@@ -11,7 +11,6 @@ import com.ricy40.caerula.entity.ModSensorTypes;
 import com.ricy40.caerula.entity.custom.seacow.behaviors.*;
 import com.ricy40.caerula.tags.ModTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.Unit;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -22,7 +21,6 @@ import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.sensing.SensorType;
-import net.minecraft.world.entity.animal.axolotl.AxolotlAi;
 import net.minecraft.world.entity.schedule.Activity;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
@@ -36,7 +34,7 @@ public class SeacowAi {
     private static final float SPEED_MULTIPLIER_WHEN_MAKING_LOVE = 0.2F;
     private static final float SPEED_MULTIPLIER_WHEN_IDLING_IN_WATER = 0.5F;
     private static final float SPEED_MULTIPLIER_WHEN_FOLLOWING_ADULT_IN_WATER = 0.6F;
-    private static final float SPEED_MULTIPLIER_WHEN_FLEEING = 1.2F;
+    private static final float SPEED_MULTIPLIER_WHEN_FLEEING = 2.0F;
     public static final List<? extends SensorType<? extends Sensor<? super Seacow>>> SENSOR_TYPES = List.of(
             SensorType.NEAREST_LIVING_ENTITIES,
             SensorType.HURT_BY,
@@ -79,7 +77,7 @@ public class SeacowAi {
 
     private static void initCoreActivity(Brain<Seacow> brain) {
         brain.addActivity(Activity.CORE, 0, ImmutableList.of(
-                new AnimalPanic(2.0F),
+                new AnimalPanic(SPEED_MULTIPLIER_WHEN_FLEEING),
                 new LookAtTargetSink(35, 60),
                 new MoveToTargetSink(),
                 new CountDownCooldownTicks(MemoryModuleType.TEMPTATION_COOLDOWN_TICKS)
@@ -91,13 +89,13 @@ public class SeacowAi {
         brain.addActivity(Activity.IDLE, 10, ImmutableList.of(
                 new TryToLocateFood(),
                 new RunSometimes<>(new SetEntityLookTarget(EntityType.PLAYER, 6.0F), UniformInt.of(30, 60)),
-                new AnimalMakeLove(ModEntityTypes.SEACOW.get(), 0.2F),
+                new AnimalMakeLove(ModEntityTypes.SEACOW.get(), SPEED_MULTIPLIER_WHEN_MAKING_LOVE),
                 new RunOne<>(ImmutableList.of(
                         Pair.of(new FollowTemptation(SeacowAi::getSpeedModifier), 1),
-                        Pair.of(new BabyFollowAdult<>(ADULT_FOLLOW_RANGE, SeacowAi::getSpeedModifierFollowingAdult), 1),
+                        Pair.of(new BabyFollowAdult<>(ADULT_FOLLOW_RANGE, SPEED_MULTIPLIER_WHEN_FOLLOWING_ADULT_IN_WATER), 1),
                         Pair.of(new Sniffling<>(), 3))),
                 new GateBehavior<>(ImmutableMap.of(MemoryModuleType.WALK_TARGET, MemoryStatus.VALUE_ABSENT), ImmutableSet.of(), GateBehavior.OrderPolicy.ORDERED, GateBehavior.RunningPolicy.TRY_ALL, ImmutableList.of(
-                        Pair.of(new RandomSwim(0.5F), 2),
+                        Pair.of(new RandomSwim(SPEED_MULTIPLIER_WHEN_IDLING_IN_WATER), 2),
                         Pair.of(new SetWalkTargetFromLookTarget(SeacowAi::canSetWalkTargetFromLookTarget, SeacowAi::getSpeedModifier, 3), 3),
                         Pair.of(new RunIf<>(Entity::isInWaterOrBubble, new DoNothing(30, 60)), 5)))));
     }
@@ -133,12 +131,8 @@ public class SeacowAi {
         }
     }
 
-    private static float getSpeedModifierFollowingAdult(LivingEntity entityIn) {
-        return entityIn.isInWaterOrBubble() ? 0.6F : 0.15F;
-    }
-
     private static float getSpeedModifier(LivingEntity entityIn) {
-        return entityIn.isInWaterOrBubble() ? 0.5F : 0.15F;
+        return SPEED_MULTIPLIER_WHEN_IDLING_IN_WATER;
     }
 
     public static Ingredient getTemptations() {
@@ -146,7 +140,7 @@ public class SeacowAi {
     }
     
     public static void setFoodLocation(Seacow seacow, BlockPos pos) {
-        if (seacow.level.getWorldBorder().isWithinBounds(pos) && !seacow.getBrain().getMemory(ModMemoryModuleTypes.FOOD_POS.get()).isPresent()) {
+        if (seacow.level.getWorldBorder().isWithinBounds(pos) && seacow.getBrain().getMemory(ModMemoryModuleTypes.FOOD_POS.get()).isEmpty()) {
 
             seacow.getBrain().setMemoryWithExpiry(MemoryModuleType.LOOK_TARGET, new BlockPosTracker(pos), 100L);
             seacow.getBrain().setMemoryWithExpiry(ModMemoryModuleTypes.FOOD_POS.get(), pos, 100L);
